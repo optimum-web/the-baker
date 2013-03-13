cf574662f7e2ddb6c28f19a67ef497081a26947f
No-Mad <nomad@cybermercindustries.com>
2013-03-12 23:30:33 -0500

    updated note fields

--- fabfile.py
+++ fabfile.py
@@ -13,28 +13,30 @@ def general_info():
 
 
 def yum_update():
-    '''Update all RPM packages'''
+    '''RedHat update all packages'''
     run("yum update -y")
 
 
 def apt_get_upgrade():
-    '''apt-get upgrade of ubuntu server'''
+    '''Ubuntu apt-get update/upgrade'''
     run("apt-get update")
     run("apt-get upgrade -y")
 
 
 def rh_ntpd_setup():
+    '''RedHat ntpd server setup on Xen VM'''
     run("echo xen.independent_wallclock = 1 >> /etc/sysctl.conf")
     run("chkconfig ntpd on")
     run("service ntpd restart")
 
 
-def rh_timezone_setup():
+def rh_set_timezone_central():
+    '''RedHat set timezone to central'''
     run("ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime")
 
 
 def yum_cron_setup():
-    '''Setup yum_cron script for nightly RPM updates'''
+    '''RedHat yum_cron script for nightly RPM updates'''
     run("yum install yum-cron -y")
     run("chkconfig yum-cron on")
     run("service yum-cron start")
