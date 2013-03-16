cca17c7bf6459c39530c0f0b0b97ed12b5a2061b
No-Mad <nomad@cybermercindustries.com>
2013-03-16 17:17:55 -0500

    code additions

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@
 from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
+from crypt import crypt
 
 
 def general_info():
@@ -12,27 +13,34 @@ def general_info():
     run("df -h")
 
 
+def install_epel_repo():
+    '''Setup EPEL repo'''
+    repo = 'http://download.fedoraproject.org/pub/epel/6/x86_64'
+    run("rpm -Uvh $s/epel-release-6-8.noarch.rpm" % repo)
+
+
 def yum_update():
     '''RedHat update all packages'''
     run("yum update -y")
 
 
-def apt_get_upgrade():
-    '''Ubuntu apt-get update/upgrade'''
-    run("apt-get update")
-    run("apt-get upgrade -y")
-
-
-def rh_ntpd_setup():
+def ntpd_setup():
     '''RedHat ntpd server setup on Xen VM'''
     run("echo xen.independent_wallclock = 1 >> /etc/sysctl.conf")
     run("chkconfig ntpd on")
     run("service ntpd restart")
 
 
-def rh_set_timezone_central():
-    '''RedHat set timezone to central'''
+def set_timezone_central():
+    '''RedHat set timezone to Central'''
+    run("rm -f /etc/localtime")
     run("ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime")
+
+
+def set_timezone_eastern():
+    '''RedHat set timezone to Eastern'''
+    run("rm -f /etc/localtime")
+    run("ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime")
 
 
 def yum_cron_setup():
@@ -46,3 +54,48 @@ def motd_setup():
     '''Install prefab MotD file'''
     run("mv /etc/motd /etc/motd.old")
     put("./files/motd", "/etc/motd")
+
+
+def create_user(user):
+    '''Create new User account'''
+    run("adduser %s" % user)
+
+
+def change_password(user):
+    '''Change user account password'''
+    password = prompt('Enter a new password for user %s:' % user)
+    crypted_password = crypt(password, 'salt')
+    run('usermod -p %s %s' % (crypted_password, user), pty=False)
+
+
+def setup_ssh_keyless_entry(user):
+    '''Install inital SSH key pairs for user access'''
+    run("mkdir /home/%s/.ssh" % user)
+    run("chmod 700 /home/%s/.ssh" % user)
+    put("./files/id_rsa.pub", "/home/%s/.ssh/authorized_keys" % user)
+    run("chmod 600 /home/%s/.ssh/authorized_keys" % user)
+
+
+def setup_logwatch():
+    '''Install and Configure Logwatch'''
+    run("yum install logwatch -y")
+    run("cd /usr/share/logwatch/default.conf/")
+    run("mv logwatch.conf logwatch.conf.old")
+    put("./files/logwatch.conf /usr/share/logwatch/default.conf/logwatch.conf")
+
+
+def ssh_lockdown():
+    '''Configure SSHd to my liking'''
+    run("mv /etc/ssh/sshd_config /etc/ssh/sshd_config.old")
+    put("./files/sshd_config /etc/ssh/sshd_config")
+    run("server sshd restart")
+
+
+def add_user_to_sudo(user):
+    '''Add a user to the sudoer file'''
+    run("echo '%s ALL=(ALL) ALL' >> /etc/sudoers" % user)
+
+
+def install_fail2ban():
+    '''Install Fail2Ban (requires EPEL)'''
+    run("yum install fail2ban -y")
